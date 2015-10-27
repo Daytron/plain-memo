47ddf1b9792d8dac74a824fa4e6ef4f204303cb6
Daytron <ryangilera@gmail.com>
2015-10-27 11:37:55 +0000

    Prepare base view

--- app/src/main/java/com/github/daytron/plain_memo/NoteListActivity.java
+++ app/src/main/java/com/github/daytron/plain_memo/NoteListActivity.java
@@ -18,7 +18,7 @@ public class NoteListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
