0ac039235d0e0e654f1fd33e04ca81ba560dc556
Daytron <ryangilera@gmail.com>
2015-10-28 18:16:40 +0000

    Add date utility class

---
+++ app/src/main/java/com/github/daytron/plain_memo/util/DateUtil.java
@@ -0,0 +1,23 @@
+package com.github.daytron.plain_memo.util;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ryan on 28/10/15.
+ */
+public class DateUtil {
+
+    private DateUtil(){}
+
+    public static String getTimeStringLocale(Context context, Date date) {
+        return DateFormat.getTimeFormat(context).format(date);
+    }
+
+    public static String getDateStringLocale(Context context, Date date) {
+        return DateFormat.getDateFormat(context).format(date);
+    }
+}
