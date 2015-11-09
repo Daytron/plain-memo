d2541cc4a330002e307c66b6fc05641e8bc88cc6
Daytron <ryangilera@gmail.com>
2015-11-09 23:56:44 +0000

    Add support to Android Testing Support Library and implement test for creating new note for issue #19

--- app/src/androidTest/java/com/github/daytron/plain_memo/ApplicationTest.java
+++ app/src/androidTest/java/com/github/daytron/plain_memo/ApplicationTest.java
@@ -1,13 +1,66 @@
 package com.github.daytron.plain_memo;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+import android.test.InstrumentationTestCase;
+
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends InstrumentationTestCase {
+
+    private static final String APP_PACKAGE = NoteListActivity.class.getPackage().getName();
+
+    private static final long LAUNCH_TIMEOUT = 10000;
+    private static final long UI_TIMEOUT = 10000;
+
+    private UiDevice mDevice;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Launch app via Intent
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(APP_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(APP_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    public void testAddNewNoteViaFabButton() throws Exception {
+        // Given
+        String expResult = "New Note";
+
+        // When
+        // Locate and click the add new note fab button
+        UiObject2 fabButton = mDevice.findObject(By.res(APP_PACKAGE, "fab_add"));
+        fabButton.click();
+
+        mDevice.wait(Until.hasObject(By.text("New Note")), UI_TIMEOUT);
+
+        // Then
+        // Get text of the title toolbar as reference to verify that the
+        // NoteEditFragment is displayed in the screen
+        UiObject2 titleInToolbar = mDevice.findObject(By.text("New Note"));
+        String titleText = titleInToolbar.getText();
+
+        assertEquals(expResult, titleText);
     }
 }
