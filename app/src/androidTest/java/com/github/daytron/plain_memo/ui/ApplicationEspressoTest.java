ba34311cea3c4f9523b35e9ba48b8340760ed054
Daytron <ryangilera@gmail.com>
2015-11-10 11:40:21 +0000

    Switch to Espresso instrumentation test for events that takes place only inside the app. For multi-apps events such as sharing text use UiAutomator. Issues #19 and #20

---
+++ app/src/androidTest/java/com/github/daytron/plain_memo/ui/ApplicationEspressoTest.java
@@ -0,0 +1,63 @@
+package com.github.daytron.plain_memo.ui;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import com.github.daytron.plain_memo.NoteListActivity;
+import com.github.daytron.plain_memo.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by ryan on 10/11/15.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationEspressoTest {
+
+    @Rule
+    public ActivityTestRule<NoteListActivity> mActivityRule =
+            new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void testIfNoteListLoadsProperly() {
+        onView(withText("Plain Memo")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testAddNewNoteViaFabButton() {
+        // Find fab button and click
+        onView(withId(R.id.fab_add)).perform(click());
+
+        // Verify the NoteEditFragment is displayed via title EditText widget
+        onView((withId(R.id.note_title_edit_text))).check(matches(isDisplayed()));
+
+        // Make sure to trigger auto delete new note for cleanup
+        pressBack();
+    }
+
+    @Test
+    public void testAddNewNoteViaMenuItem() {
+        // Find add note menu item and click
+        onView(withContentDescription("More options")).perform(click());
+        onView(withText(R.string.new_note)).perform(click());
+
+        // Verify the NoteEditFragment is displayed via title EditText widget
+        onView((withId(R.id.note_title_edit_text))).check(matches(isDisplayed()));
+
+        // Make sure to trigger auto delete new note for cleanup
+        pressBack();
+    }
+}
---
+++ app/src/androidTest/java/com/github/daytron/plain_memo/ui/ApplicationUiAutomatorTest.java
@@ -0,0 +1,64 @@
+package com.github.daytron.plain_memo.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+import android.test.InstrumentationTestCase;
+
+import com.github.daytron.plain_memo.NoteListActivity;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ApplicationUiAutomatorTest extends InstrumentationTestCase {
+
+    private static final String APP_PACKAGE = NoteListActivity.class.getPackage().getName();
+
+    private static final long LAUNCH_TIMEOUT = 10000;
+    private static final long UI_TIMEOUT = 15000;
+
+    private UiDevice mDevice;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Launch app via Intent
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(APP_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(APP_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkPreconditions() {
+        assertThat(mDevice, notNullValue());
+    }
+
+
+
+}
