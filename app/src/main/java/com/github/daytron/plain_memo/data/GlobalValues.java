04991f4ae0f42e64d68331cd35cf1bfffbdaca62
Daytron <ryangilera@gmail.com>
2015-11-17 21:59:52 +0000

    Reword welcome message

--- app/src/main/java/com/github/daytron/plain_memo/data/GlobalValues.java
+++ app/src/main/java/com/github/daytron/plain_memo/data/GlobalValues.java
@@ -11,16 +11,15 @@ public class GlobalValues {
     public static final float FONT_SIZE_DIFFERENCE = 4.0F;
     public static final String WELCOME_TITLE = "Welcome to Plain Memo!";
     public static final String WELCOME_BODY = "A simple note app for all your text storage needs."
-            + "\n\nThis is a beta release. Please feel free to submit any issues or questions "
-            + "to gilera.ryan@gmail.com."
+            + "\n\nPlease feel free to submit any issues or questions "
+            + "you might have via Send Feedback section in Settings > General."
             + "\n\nFEATURES:"
-            + "\n1. Create, edit, and delete notes."
+            + "\n1. Create, view, edit, and delete notes."
             + "\n2. Search and filter notes easily."
-            + "\n3. Single touch edit - allowing you to initiate edit easily. Simply "
-            + "tap on the location of the text you would like to edit."
-            + "\n4. Allows font size change."
-            + "\n5. Support for large screen and tablet devices."
-            + "\n6. Share your notes to your favourite apps."
-            + "\n7. And save notes from your favourite apps (if share enabled)."
+            + "\n3. Single touch edit. While in Note view screen, simply "
+            + "tap on the location of the note text you would like to edit."
+            + "\n4. Change font size."
+            + "\n5. Support for large screen or tablet devices."
+            + "\n6. Share your notes to your favourite apps and vice versa."
             + "\n\nI hope you enjoy using Plain Memo.";
 }
